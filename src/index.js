fc23e551f9be706fb62929b018299dfd5f60f37a
Rico00121 <837043207@qq.com>
2022-03-11 01:08:53 +0800

    feat: learn the basic usage of react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
 import Counter from "./components/counter";
 
+const name = "Rico";
+const age = 18;
+const func = () => "heeeee!";
+const title = (
+  <h1 className="hi">
+    Hello JSX,<span>{name}</span>,{age}岁的男人
+    {func()}
+  </h1>
+);
 
-ReactDOM.render(<Counter />,document.getElementById('root'))
+ReactDOM.render(title, document.getElementById("root"));
