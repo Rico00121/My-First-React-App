f5af1765d28b2cb5ed6e4f251c023888b0c9cf8f
Rico00121 <837043207@qq.com>
2022-03-13 20:23:11 +0800

    feat: JSX中的样式处理 条件渲染 列表渲染

--- src/index.js
+++ src/index.js
@@ -2,15 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Counter from "./components/counter";
+//引入css
+import "./css/index.css";
 
 const name = "Rico";
 const age = 18;
+const isLoading = false;
 const func = () => "heeeee!";
-const title = (
+const loadData = () => {
+  if (isLoading) {
+    return <h1>loading======</h1>;
+  }
+  return <div>数据加载失败</div>;
+};
+
+const songs = [
+  { id: 1, name: "rico" },
+  { id: 1, name: "syc" },
+  { id: 1, name: "sky" },
+  { id: 1, name: "sry" },
+];
+const list = (
+  <ul>
+    {songs.map((item) => (
+      <li key={item.id}>{item.name}</li>
+    ))}
+  </ul>
+);
+
+const body = (
   <h1 className="hi">
     Hello JSX,<span>{name}</span>,{age}岁的男人
     {func()}
+    {loadData()}
+    {list}
   </h1>
 );
 
-ReactDOM.render(title, document.getElementById("root"));
+ReactDOM.render(body, document.getElementById("root"));
